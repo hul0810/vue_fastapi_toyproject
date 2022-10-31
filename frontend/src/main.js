6547bb019f152b8a3abb6d5bcb41d0d52f0313aa
hul0810 <hul0810@naver.com>
2022-10-31 11:02:18 +0000

    add bootstrap library

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,6 +4,18 @@ import router from '@/router/index';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+// Fontawesome library
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+// Fontawesome library - all icon
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+
+library.add(fas, far, fab);
+Vue.component('font-awesome-icon', FontAwesomeIcon);
+
 Vue.config.productionTip = false;
 
 new Vue({
